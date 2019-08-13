0efd4c70544c7ee397d95d91d93346fccfb41b12
Alexander Einshøj Lund <alexander.lund1@gmail.com>
2019-08-13 18:45:52 +0200

    working on user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,11 @@
-require_relative "../lib/board.rb"
-require_relative "../lib/player.rb"
+require_relative '../lib/board.rb'
+require_relative '../lib/player.rb'
+require_relative '../lib/messages.rb'
+require 'colorize'
 weapon = ""
 game = Board.new
-current_player = 1
 
-puts "Welcome to Tic-Tac-Toe"
-puts "Press enter to start"
-gets
+Messages.welcome
 
 puts "Enter name for player 1"
 name = gets.chomp
@@ -28,22 +27,28 @@ player2 = Player.new(name, weapon)
 game.display_board
 
 5.times do
-  puts "#{player1.name} choose a number from 0 to 8 to place your weapon"
+  Messages.player
   player_choice = gets.chomp.to_i
 
-  game.board[player_choice] = player1.weapon
+  if game.board[player_choice].is_a? Integer
+    game.board[player_choice] = player1.weapon.red 
+  else
+    Messages.player
+    player_choice = gets.chomp.to_i
+  end
+
   game.display_board
 
-  puts "#{player2.name} Choose a number from 0 to 8 to place your weapon"
+  Messages.player
   player_choice = gets.chomp.to_i
 
-  game.board[player_choice] = player2.weapon
-  game.display_board 
+  if game.board[player_choice].is_a? Integer
+    game.board[player_choice] = player2.weapon.blue
+  else
+    Messages.player
+    player_choice = gets.chomp.to_i
+  end
+  game.display_board
 end
-
-
-
-
-
 
 
