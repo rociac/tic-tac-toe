5e3007e460f5b0c8a60b65db21fc21ddc9e0f8c4
Rodolfo Cisneros <brsrkdev@gmail.com>
2019-08-19 10:57:08 -0500

    Add files for rspec and test for player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/player.rb'
+
+RSpec.describe Player do
+  let(:player) {described_class.new('player1', 'X')}
+
+  it 'should have a name' do
+    expect(player.name).to eql('player1')
+  end
+
+  it 'should have a weapon' do
+    expect(player.weapon).to eql('X')
+  end
+end
